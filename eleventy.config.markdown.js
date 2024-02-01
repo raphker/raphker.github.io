6b41ac2798851ba6cde3c5b05017b8f7f00a4fdf
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-02 00:24:15 +0100

    without ":has" selector

--- eleventy.config.markdown.js
+++ eleventy.config.markdown.js
@@ -1,9 +1,40 @@
 const eleventyImage = require("@11ty/eleventy-img");
 const path = require("node:path");
 
+function imagesWrapperPlugin(md, options) {
+  options = options || {};
+
+  function imagesWrapper(state) {
+    for (let i = 1; i < state.tokens.length - 1; i++) {
+      const prevToken = state.tokens[i - 1];
+      const token = state.tokens[i];
+      const nextToken = state.tokens[i + 1];
+      if (token.type !== "inline") {
+        continue;
+      }
+      if (
+        prevToken.type !== "paragraph_open" ||
+        nextToken.type !== "paragraph_close"
+      )
+        continue;
+      if (
+        token.children.some(
+          (el) => el.type !== "image" && el.type !== "softbreak"
+        )
+      )
+        continue;
+      state.tokens[i - 1].attrPush(["class", "imagesRow"]);
+    }
+  }
+
+  md.core.ruler.before("linkify", "images_wrapper", imagesWrapper);
+}
+
 /** @param {import('@11ty/eleventy').UserConfig} config */
 module.exports = function (config) {
   config.amendLibrary("md", (mdLib) => {
+    mdLib.use(imagesWrapperPlugin);
+
     mdLib.renderer.rules.image = function (tokens, idx) {
       const token = tokens[idx];
       const file = token.attrGet("src");
