429935a77a541c10a08e295e9cefc0ac4dd35e84
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-03 11:51:21 +0100

    hamburger menu anim

--- content/scripts/hamburger.ts
+++ content/scripts/hamburger.ts
@@ -1,13 +1,13 @@
 import gsap from "gsap";
-import { trapFocus } from "./lib/trapFocus.js";
 
 export class Hamburger extends HTMLElement {
   button?: HTMLElement;
   list?: HTMLElement;
   path?: SVGPathElement;
   expanded = false;
+  openingTl = gsap.timeline({ paused: true });
+  closingTl = gsap.timeline({ paused: true });
 
-  stopTrappingFocus?: () => void;
   constructor() {
     super();
   }
@@ -22,12 +22,36 @@ export class Hamburger extends HTMLElement {
     this.list = nav;
     this.path = path;
 
+    this.openingTl
+      .to(this.path!, {
+        attr: {
+          d: "M6 18 L 18 6 M6 6l12 12",
+        },
+        duration: 0.3,
+      })
+      .to(
+        this.list!,
+        {
+          opacity: 1,
+          duration: 0.3,
+        },
+        "<"
+      )
+      .from(this.list!.querySelectorAll("a"), {
+        yPercent: 100,
+        duration: 0.2,
+        stagger: 0.1,
+        ease: "ease.in",
+      });
+    this.list.style.setProperty("opacity", "0");
+
     this.button.setAttribute("aria-expanded", this.expanded.toString());
     this.button.addEventListener("click", this.toggle);
   }
   disconnectedCallback() {
     this.button?.removeEventListener("click", this.toggle);
-    if (this.stopTrappingFocus) this.stopTrappingFocus();
+    this.openingTl.kill();
+    this.closingTl.kill();
   }
 
   toggle = () => {
@@ -37,21 +61,15 @@ export class Hamburger extends HTMLElement {
 
   async open() {
     this.expanded = true;
+    this.openingTl.restart();
     document.body.style.setProperty("overflow", "hidden");
-    this.stopTrappingFocus = trapFocus(this);
-    gsap.to(this.path!, {
-      attr: {
-        d: "M6 18 L 18 6 M6 6l12 12",
-      },
-      duration: 0.3,
-    });
-
     this.button?.setAttribute("aria-expanded", this.expanded.toString());
     this.list?.style.setProperty("display", "flex");
   }
 
   close() {
     this.expanded = false;
+    this.button?.setAttribute("aria-expanded", this.expanded.toString());
     document.body.style.setProperty("overflow", "auto");
     gsap.to(this.path!, {
       attr: {
@@ -59,8 +77,13 @@ export class Hamburger extends HTMLElement {
       },
       duration: 0.3,
     });
-    if (this.stopTrappingFocus) this.stopTrappingFocus();
-    this.button?.setAttribute("aria-expanded", this.expanded.toString());
-    this.list?.style.setProperty("display", "none");
+    gsap
+      .to(this.list!, {
+        opacity: 0,
+        duration: 0.6,
+      })
+      .then(() => {
+        this.list?.style.setProperty("display", "none");
+      });
   }
 }
