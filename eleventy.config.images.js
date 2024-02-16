2201c58d9df0409251fd469659a986d5350ef893
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-16 13:47:29 +0100

    images sizes

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -3,22 +3,31 @@ const path = require("path");
 
 /** @param {import('@11ty/eleventy').UserConfig} config */
 module.exports = function (config) {
-  config.addShortcode("image", async (src, alt) => {
-    // remove "/", if existing, from the start of the filePath
-    const imgPath = src.replace(/^\//, "");
-    const imageMetadata = await Image(imgPath, {
-      widths: ["auto"],
-      formats: ["webp", "jpeg"],
-      outputDir: path.join(config.dir.output, "img"),
-      urlPath: "/img",
-    });
+  config.addShortcode(
+    "image",
+    async (src, alt, widths = ["auto"], sizes = undefined, lazy = true) => {
+      // remove "/", if existing, from the start of the filePath
+      const imgPath = src.replace(/^\//, "");
 
-    const imageAttributes = {
-      alt,
-      loading: "lazy",
-      decoding: "async",
-    };
+      const imageMetadata = await Image(imgPath, {
+        widths,
+        formats: ["webp", "jpeg"],
+        outputDir: path.join(config.dir.output, "img"),
+        urlPath: "/img",
+      });
 
-    return Image.generateHTML(imageMetadata, imageAttributes);
-  });
+      let imageAttributes = {
+        alt,
+        sizes,
+      };
+      if (lazy)
+        imageAttributes = {
+          ...imageAttributes,
+          loading: "lazy",
+          decoding: "async",
+        };
+
+      return Image.generateHTML(imageMetadata, imageAttributes);
+    }
+  );
 };
