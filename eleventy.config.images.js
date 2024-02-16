612abf0552e6f762e5d244a49f0b4114b72eadcb
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-16 14:40:42 +0100

    process Images by height

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -1,6 +1,7 @@
 const Image = require("@11ty/eleventy-img");
 const path = require("path");
 
+let logged = false;
 /** @param {import('@11ty/eleventy').UserConfig} config */
 module.exports = function (config) {
   config.addShortcode(
@@ -19,6 +20,44 @@ module.exports = function (config) {
       let imageAttributes = {
         alt,
         sizes,
+      };
+      if (lazy)
+        imageAttributes = {
+          ...imageAttributes,
+          loading: "lazy",
+          decoding: "async",
+        };
+
+      return Image.generateHTML(imageMetadata, imageAttributes);
+    }
+  );
+  config.addShortcode(
+    "image_by_height",
+    /**
+     * @param {string} src
+     * @param {string} alt
+     * @param {number} height
+     * @returns
+     */
+    async (src, alt, height, lazy = false) => {
+      // remove "/", if existing, from the start of the filePath
+      const imgPath = src.replace(/^\//, "");
+      const [rawData] = Image.statsSync(imgPath, {
+        widths: ["auto"],
+        formats: "jpeg",
+        urlPath: "/img",
+      }).jpeg;
+      const imageRatio = rawData.width / rawData.height;
+
+      const imageMetadata = await Image(imgPath, {
+        widths: [height * imageRatio],
+        formats: ["webp", "jpeg"],
+        outputDir: path.join(config.dir.output, "img"),
+        urlPath: "/img",
+      });
+
+      let imageAttributes = {
+        alt,
       };
       if (lazy)
         imageAttributes = {
