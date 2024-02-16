995ac6418539af930bcf75ca6d391c3517dfd28c
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-16 13:53:05 +0100

    feat metadescription

--- data/config.js
+++ data/config.js
@@ -2,4 +2,6 @@ module.exports = {
   defaultLanguage: "fr",
   title: "Raphaëlle Kerbrat",
   copyright: "©2024 Raphaëlle Kerbrat",
+  metaDescription:
+    "Portfolio de Raphaëlle Kerbrat, artiste plasticienne et docteure SACRe",
 };
