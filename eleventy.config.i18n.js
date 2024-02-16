87886384fd85fbbf8c88f5ce326f45b38df6f569
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-16 12:09:43 +0100

    feat njkFilter "filter_by_lang"

--- eleventy.config.i18n.js
+++ eleventy.config.i18n.js
@@ -11,25 +11,33 @@ module.exports = function (config) {
     "favor_lang",
     /**
      * @param {Record<string,any>[]} collection
-     * @param {string} langOverride
-     * @returns
+     * @param {string} [langOverride]
      */
     function (collection, langOverride) {
       const lang = langOverride || this.page.lang;
       const defaults = collection.filter(
         (el) => el.data.lang === siteConfig.defaultLanguage
       );
-      const getFileSlug = (el) =>
-        el.fileSlug === el.data.lang ? "index" : el.fileSlug;
       return defaults.map((el) => {
         const re = new RegExp(`^\/${siteConfig.defaultLanguage}\/`);
         const target = el.filePathStem.replace(re, `/${lang}/`);
-        const slug = getFileSlug(el);
         const currentLangItem = collection.find(
           (el) => el.filePathStem === target && el.data.lang === lang
         );
         return currentLangItem ?? { ...el, isFallback: true };
       });
+    }
+  );
+
+  config.addFilter(
+    "filter_by_lang",
+    /**
+     * @param {Record<string,any>[]} collection
+     * @param {[string]} lang
+     */
+    function (collection, lang) {
+      const currentLang = lang || this.page.lang;
+      return collection.filter((el) => el.data.lang === currentLang);
     }
   );
 };
